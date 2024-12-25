a848f2e16d09205e046b173cc50fc328e5301457
mazlike <daniil.maslike@ya.ru>
2024-12-25 15:05:48 +0000

    Resolved merge conflicts and updated user-related files

--- users/forms.py
+++ users/forms.py
@@ -17,7 +17,6 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
-    
 class ProfileForm(UserChangeForm):
     class Meta:
         model = User
